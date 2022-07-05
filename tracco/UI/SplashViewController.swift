5f392bc99148fa6bcbb150d7de3ddd563d70e8c3
ramdanks <ramadhan.kalih@ui.ac.id>
2022-07-06 03:37:10 +0700

    style: set light content status bar

--- tracco/UI/SplashViewController.swift
+++ tracco/UI/SplashViewController.swift
@@ -9,38 +9,31 @@ import UIKit
 
 class SplashViewController: UIViewController
 {
+    override var preferredStatusBarStyle: UIStatusBarStyle { return .lightContent }
+    
     @IBOutlet weak var animationImageView: UIImageView!
 
     public var completion: (() -> Void)?
-    
-    private let targetFPS: Double = 60.0
-    
-    private let imageSequence: [UIImage] = {
-        // load images
-        let indexStart = 1
-        let indexStop = 70
-        let sequenceLength = indexStop - indexStart
-        
-        let assetFormat = "AAA_%05d"
-        
-        var images: [UIImage] = []
-        images.reserveCapacity(sequenceLength)
-        
-        for i in indexStart...indexStop
-        {
-            let assetName = String(format: assetFormat, i)
-            let image = UIImage(named: assetName)
-            images.append(image!)
-        }
-        
-        return images
-    }()
     
     override func viewDidLoad()
     {
         super.viewDidLoad()
         
         if completion == nil { completion = { self.dismiss(animated: true) } }
+        
+        let targetFPS: Double = 60.0
+        let imageSequence: [UIImage] = {
+            let assetFormat = "AAA_%05d"
+            var images: [UIImage] = []
+            images.reserveCapacity(100)
+            for i in 1...70
+            {
+                let assetName = String(format: assetFormat, i)
+                let image = UIImage(named: assetName)!
+                images.append(image)
+            }
+            return images
+        }()
         
         animationImageView.animationImages = imageSequence
         animationImageView.animationDuration = Double(imageSequence.count) / targetFPS
