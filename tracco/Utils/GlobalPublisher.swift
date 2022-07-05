3c363645a61d0f008130322c04d4b0605150418a
ramdanks <ramadhan.kalih@ui.ac.id>
2022-07-05 11:20:45 +0700

    feat: user local notification

--- tracco/Utils/GlobalPublisher.swift
+++ tracco/Utils/GlobalPublisher.swift
@@ -6,12 +6,19 @@
 //
 
 import Foundation
+import CoreLocation
 
 protocol GlobalEvent
 {
     func tripModelAdded(_ model: TripModel)
     func tripModelUpdated(_ model: TripModel)
     func profileModelUpdated(_ model: ProfileModel)
+    
+    // MARK: Triggered by OnTripViewController
+    
+    // feed by CLLocationManagerDelegate (can be in background)
+    func onTripLocationUpdate(_ locations: [CLLocation])
+    func onTripLocationLost(_ manager: CLLocationManager)
     
     func onTripTransitModelUpdated(_ model: TransitModel)
     func onTripEnded()
@@ -24,7 +31,8 @@ extension GlobalEvent
     func tripModelAdded(_ model: TripModel) {}
     func tripModelUpdated(_ model: TripModel) {}
     func profileModelUpdated(_ model: ProfileModel) {}
-    
+    func onTripLocationUpdate(_ locations: [CLLocation]) {}
+    func onTripLocationLost(_ manager: CLLocationManager) {}
     func onTripTransitModelUpdated(_ model: TransitModel) {}
     func onTripEnded() {}
     func onTripStarted() {}
@@ -69,6 +77,16 @@ class GlobalPublisher: GlobalEvent
         publish { $0.tripModelUpdated(model) }
     }
     
+    func onTripLocationUpdate(_ locations: [CLLocation])
+    {
+        publish { $0.onTripLocationUpdate(locations) }
+    }
+    
+    func onTripLocationLost(_ manager: CLLocationManager)
+    {
+        publish { $0.onTripLocationLost(manager) }
+    }
+    
     func onTripTransitModelUpdated(_ model: TransitModel)
     {
         publish { $0.onTripTransitModelUpdated(model) }
@@ -83,7 +101,7 @@ class GlobalPublisher: GlobalEvent
     {
         publish { $0.onTripEnded() }
     }
-    
+
     private func publish(_ callback: (GlobalEvent) -> Void)
     {
         GlobalPublisher.observers.forEach {
